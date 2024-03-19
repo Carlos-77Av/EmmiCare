2e43cb7fd26fcd8506e4f5e1f35173a90cf98c7f
Carlos-77Av <carlos.av.bit@gmail.com>
2024-03-18 23:01:45 -0600

    🎉 Initial project

---
+++ EmmiApp/EmmiApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmmiApp
+//
+//  Created by Carlos Alvarez on 18/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmmiApp/EmmiApp/EmmiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmmiAppApp.swift
+//  EmmiApp
+//
+//  Created by Carlos Alvarez on 18/3/24.
+//
+
+import SwiftUI
+
+@main
+struct EmmiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
